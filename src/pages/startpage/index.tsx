536d580ec04700cf48d8bc39bd9b3daa89110ac8
GabrielDevXD1 <84517125+GabrielDevXD@users.noreply.github.com>
2022-09-05 12:50:31 -0300

    update(project): start-page-styled

--- src/pages/startpage/index.tsx
+++ src/pages/startpage/index.tsx
@@ -11,6 +11,29 @@ const StartPage = () => {
           <p>Letras</p>
         </S.iconConfig>
       </S.heading>
+      <S.content>
+        <S.text1>kinako viado</S.text1>
+        <S.text2>kinako viado</S.text2>
+        <S.text3>
+          kinako viado kinako viado kinako viado kinako viado kinako viado
+          kinako viadokinako viado kinako viado kinako viado kinako viado kinako
+          viado kinako viado
+        </S.text3>
+      </S.content>
+      <S.primaryFooter>
+        <S.divFooter>
+          <h5>Empresa</h5>
+        </S.divFooter>
+        <S.divFooter>
+          <h5>Contato</h5>
+        </S.divFooter>
+        <S.divFooter>
+          <h5>FAQ</h5>
+        </S.divFooter>
+      </S.primaryFooter>
+      <S.secundaryFooter>
+        <p>© 2022 • Letras kinakos • Todos os direitos reservados </p>
+      </S.secundaryFooter>
     </S.startpages>
   );
 };
--- src/pages/startpage/style.ts
+++ src/pages/startpage/style.ts
@@ -41,3 +41,107 @@ export const heading = styled.div`
     justify-content: center;
   `}
 `;
+export const primaryFooter = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    width: 100%;
+
+    background: ${theme.colors.footerPrimaryColor};
+    align-items: center;
+    justify-content: space-around;
+  `}
+`;
+
+export const secundaryFooter = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    width: 100%;
+
+    background: ${theme.colors.footerSecundaryColor};
+
+    p {
+      width: 100%;
+      color: ${theme.colors.text};
+      font-family: ${theme.constants.headingFontFamily};
+      font-size: 0.7rem;
+      font-weight: 400;
+      text-align: center;
+    }
+  `}
+`;
+export const divFooter = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    width: 25%;
+    flex-direction: column;
+    align-items: center;
+    flex-wrap: wrap;
+
+    h5 {
+      color: ${theme.colors.text};
+      font-family: ${theme.constants.textInitialFontFamily};
+      font-weight: 800;
+      font-size: 1rem;
+    }
+    p {
+      color: ${theme.colors.text};
+      font-family: ${theme.constants.textInitialFontFamily};
+      font-weight: 500;
+      font-size: 0.8rem;
+      margin-top: -2rem;
+      transition: ease 0.5s;
+    }
+    p:hover {
+      transform: scale(1.1);
+      cursor: pointer;
+    }
+  `}
+`;
+export const content = styled.main`
+  display: flex;
+  min-width: 100%;
+  align-items: center;
+  justify-content: center;
+
+  height: 100vh;
+  flex-direction: column;
+`;
+
+export const text1 = styled.p`
+  ${({ theme }) => css`
+    display: flex;
+
+    color: ${theme.colors.text};
+    font-family: ${theme.constants.textInitialFontFamily};
+    font-size: 200%;
+    margin: 0;
+    font-weight: 600;
+    margin-bottom: -3rem;
+  `}
+`;
+
+export const text2 = styled.p`
+  ${({ theme }) => css`
+    display: flex;
+
+    font-size: 330%;
+    color: ${theme.colors.text};
+    font-family: ${theme.constants.textInitialFontFamily};
+    font-weight: 800;
+    text-align: center;
+  `}
+`;
+
+export const text3 = styled.p`
+  ${({ theme }) => css`
+    margin: 0;
+    width: 50%;
+    display: flex;
+
+    margin-top: -2rem;
+    text-align: center;
+    color: ${theme.colors.text};
+    font-family: ${theme.constants.textFinalFontFamily};
+    font-weight: 400;
+  `}
+`;
