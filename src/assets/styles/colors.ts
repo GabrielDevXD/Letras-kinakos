8938ffa29d085b6cfbf4249264a8c4d765a15777
GabrielDevXD1 <84517125+GabrielDevXD@users.noreply.github.com>
2022-09-05 11:51:40 -0300

    feat(project): add-syled-components

---
+++ src/assets/styles/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  primaryColors: "#FFFFFF",
+  secundaryColor: "#2EA8DC",
+  backgroundColor: "#D9D9D9",
+  textColorBody: "#000000",
+  footerPrimaryColor: "#1E2325",
+  footerSecundaryColor: "#4A5B63",
+  shadowColorHover: "#E3DFEB",
+};
---
+++ src/assets/styles/constants.ts
@@ -0,0 +1,14 @@
+export const constants = {
+  headingFontFamily: "Poppins",
+  headingFontSize: "20px",
+  headingLineHeight: "140%",
+
+  bodyFontSize: "15px",
+  bodyLineHeight: "140%",
+
+  textInitialFontFamily: "Nunito",
+
+  textFinalFontFamily: "Montserrat",
+
+  nameInstitutionFamily: "Bebas Neue",
+};
